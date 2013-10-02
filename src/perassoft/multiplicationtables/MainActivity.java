20f60eee751859cde85f332a1a34a181f5a00bd6
Marco <marco.perasso@gmail.com>
2013-10-02 17:51:45 +0200

    cambiato balloon

--- src/perassoft/multiplicationtables/MainActivity.java
+++ src/perassoft/multiplicationtables/MainActivity.java
@@ -13,7 +13,6 @@ import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.speech.tts.TextToSpeech;
